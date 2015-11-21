271cd4089a976aa5dfa0d03a2542d09ce4924c05
bumkyu lee <bumkyu.lee@gmail.com>
2015-11-21 23:46:43 +0900

    전체 인물 정보 파싱중

--- save_historians.py
+++ save_historians.py
@@ -9,6 +9,7 @@ import json
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import random
+import string
 
 def getSoup(url):
 	opener = urllib2.build_opener()
@@ -80,6 +81,98 @@ def getUrls_People(url):
 
 # 인물 정보를 파싱한다.
 def getInfos_Person(url):
-	soup = getSoup(url)
+	try:
+		soup = getSoup(url)
+		name = soup.find('h3',attrs={'class':'tit_desc'}).text
+		desc = soup.find('strong',attrs={'class':'tit_other'})
+		desc = desc.text if desc is not None else ''
+
+		birth = 0
+		death = 0
+		nationality = ''
+
+		for row in soup.find('table',attrs={'class':'list_summary'}).findAll('tr'):
+			if row.span.text.encode('utf-8') == '출생' :
+				if row.td.text.encode('utf-8').find('BC') >= 0:
+					birth = row.td.text.encode('utf-8').replace('BC ',''),replace('.',' ').split(' ')[0]
+				else:
+					birth = row.td.text.encode('utf-8').replace('년',' ').split(' ')[0]
+			elif row.span.text.encode('utf-8') == '사망' :
+				if row.td.text.encode('utf-8').find('BC') >= 0:
+					death = row.td.text.encode('utf-8').replace('BC ',''),replace('.',' ').split(' ')[0]
+				else:
+					death = row.td.text.encode('utf-8').replace('년',' ').split(' ')[0]
+			elif row.span.text.encode('utf-8') == '국적' :
+				nationality = row.td.text.encode('utf-8')
+
+		values = [name,desc,str(birth),str(death),nationality]
+	except:
+		values = [name,'error',0,0,'error']
+	return values
+
+# 마지막페이지인지 구별한다.
+def checkEndPage(url):
+	try:
+		soup = getSoup(url)
+	except:
+		return True
+	if soup.find('span').text.encode('utf-8').find('없음') is None:
+		return True
+	else:
+		return False
+
+# 전체 인물 정보를 파싱한다.
+def getAll():
+	count = 0
+	name = list()
+	desc = list()
+	birth = list()
+	death = list()
+	nationality = list()
+	depth = list()
+
+	#카테고리 파싱
+	urls_categories = getUrls_Categories()
+	for url_category in urls_categories:
+		page = 0
+		# 테스트용
+		if count > 10:
+			break
+		#카테고리 내 페이지 파싱
+		while True:
+			# 테스트용
+			if count > 10:
+				break
+			page += 1
+			url_category = url_category+'?page='+str(page)
+			if checkEndPage(url_category):
+				break
+			#인물 파싱
+			urls_people = getUrls_People(url_category)
+			for url_person in urls_people:
+					values = getInfos_Person(url_person)
+					name.append(values[0])
+					desc.append(values[1])
+					birth.append(values[2])
+					death.append(values[3])
+					nationality.append(values[4])
+					depth.append(str(random.randrange(1,10)))
+					count += 1
+					if values[1] == 'error':
+						print str(count) + '번째, 저장 실패: ' + url_person.encode('utf-8')
+					else:
+						print str(count)+' 개 저장 완료'
+						print values
+
+	# 스프레드시트 저장
+	setColumn('A',name,'name')
+	setColumn('B',birth,'birth')
+	setColumn('C',death,'death')
+	setColumn('D',nationality,'nationality')
+	setColumn('E',depth,'depth')
+	setColumn('F',desc,'desc')
+
+	return 'Success: ' + str(count)
+
 
 
